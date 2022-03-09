ea560674555c265e0829d91e38ec7cfce775ad09
n0wsh1n <91810021+n0wsh1n@users.noreply.github.com>
2022-03-09 21:57:34 +0000

    Refactor Gamw to change the DynamicBody to a Student class that incorporates the shape and image of the original DynamicBody.

--- src/game/Student.java
+++ src/game/Student.java
@@ -1,4 +1,19 @@
 package game;
 
-public class Student {
+import city.cs.engine.BodyImage;
+import city.cs.engine.BoxShape;
+import city.cs.engine.DynamicBody;
+import city.cs.engine.Shape;
+import city.cs.engine.World;
+
+public class Student extends DynamicBody {
+    private static final Shape studentShape = new BoxShape(1,2);
+
+    private static final BodyImage image = new BodyImage("data/student.png", 4);
+
+    public Student(World world){
+        super(world, studentShape);
+        this.addImage(image);
+    }
+
 }

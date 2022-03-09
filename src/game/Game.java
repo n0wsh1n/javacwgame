d1725a9c361ec93d379df735cf05d42c0ee9c2fa
n0wsh1n <91810021+n0wsh1n@users.noreply.github.com>
2022-03-09 22:11:37 +0000

    refactor the Game class to remove the body construction and instead construct a GameWorld class to encapsulate the set of bodies/shapes in the World.

--- src/game/Game.java
+++ src/game/Game.java
@@ -21,28 +21,13 @@ public class Game {
     public Game() {
 
         //1. make an empty game world
-        World world = new World();
-
-        //2. populate it with bodies (ex: platforms, collectibles, characters)
-
-        //make a ground platform
-        Shape shape = new BoxShape(30, 0.5f);
-        StaticBody ground = new StaticBody(world, shape);
-        ground.setPosition(new Vec2(0f, -11.5f));
-
-        // make a suspended platform
-        Shape platformShape = new BoxShape(3, 0.5f);
-        StaticBody platform1 = new StaticBody(world, platformShape);
-        platform1.setPosition(new Vec2(-8, -4f));
-
-        //make a character (with an overlaid image)
-
-        Student student = new Student(world);
-        student.setPosition(new Vec2(4,-5));
+        //World world = new World();
+        GameWorld world = new GameWorld();
 
 
 
-        //3. make a view to look into the game world
+
+        //2. make a view to look into the game world
         UserView view = new UserView(world, 500, 500);
 
 
